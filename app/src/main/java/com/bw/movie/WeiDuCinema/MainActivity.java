97cca431f75faf7b1fb7318de5b8fe4f6652a5d1
JingXinJie <754337419@qq.com>
2018-11-27 10:47:14 +0800

    xixi

--- app/src/main/java/com/bw/movie/WeiDuCinema/MainActivity.java
+++ app/src/main/java/com/bw/movie/WeiDuCinema/MainActivity.java
@@ -2,7 +2,6 @@ package com.bw.movie.WeiDuCinema;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("aaa", "asdasd");
-        Log.i("aaa", "asdasd");
     }
 }
